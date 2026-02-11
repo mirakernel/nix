6ae89e9e16c1d3842d9093359c1cd2180e18df01
Mirakernel <mirakernel@disroot.org>
2026-02-12 02:08:10 +0300

    lock + bpk

--- hosts/tsunami/configuration.nix
+++ hosts/tsunami/configuration.nix
@@ -59,6 +59,7 @@
 
   home-manager.useGlobalPkgs = true;
   home-manager.useUserPackages = true;
+  home-manager.backupFileExtension = "bak";
   home-manager.sharedModules = [
     nixvim.homeModules.nixvim
     plasma-manager.homeModules.plasma-manager
