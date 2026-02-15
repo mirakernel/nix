41d2ebca57f93a20ee923440091f08ae9a42bbc3
Mirakernel <mirakernel@disroot.org>
2026-02-16 04:19:38 +0500

    kderc

--- modules/home-manager/apps.nix
+++ modules/home-manager/apps.nix
@@ -1,6 +1,10 @@
-{ pkgs, ... }: {
-  home.packages = with pkgs; [
-    telegram-desktop
-    obsidian
-  ];
+{ config, lib, pkgs, ... }: {
+  home.packages = with pkgs;
+    [
+      telegram-desktop
+      obsidian
+    ]
+    ++ lib.optionals (lib.attrByPath [ "my" "hm" "plasma" "enable" ] false config) [
+      krdc
+    ];
 }
