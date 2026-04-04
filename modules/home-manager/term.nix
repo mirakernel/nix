5418e9eb5c7934db6e1bce986039e1d2a05add3e
Mirakernel <mirakernel@disroot.org>
2026-04-04 06:38:58 +0300

    kitty x11

--- modules/home-manager/term.nix
+++ modules/home-manager/term.nix
@@ -14,6 +14,7 @@ in
         name = "JetBrainsMono Nerd Font";
       };
       settings = {
+        linux_display_server = "x11";
         shell = "${pkgs.tmux}/bin/tmux new-session -A -s main";
         background_opacity = "0.96";
       };
