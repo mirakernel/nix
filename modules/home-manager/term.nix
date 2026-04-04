0d3b98e954ef48e218568aec12f437dd1bc21dba
Mirakernel <mirakernel@disroot.org>
2026-04-04 06:54:26 +0300

    remove kitty from term module

--- modules/home-manager/term.nix
+++ modules/home-manager/term.nix
@@ -1,23 +1,10 @@
-{ config, lib, pkgs, ... }:
+{ config, lib, ... }:
 let
   cfg = config.my.hm.term;
-in
-{
+in {
   options.my.hm.term = {
     enable = lib.mkEnableOption "настройка терминала";
   };
 
-  config = lib.mkIf cfg.enable {
-    programs.kitty = {
-      enable = true;
-      font = {
-        name = "JetBrainsMono Nerd Font";
-      };
-      settings = {
-        linux_display_server = "x11";
-        shell = "${pkgs.tmux}/bin/tmux new-session -A -s main";
-        background_opacity = "0.96";
-      };
-    };
-  };
+  config = lib.mkIf cfg.enable { };
 }
